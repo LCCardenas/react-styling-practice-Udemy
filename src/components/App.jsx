a7f2d36edfa11642ea4c71890ba2346bf6ac0a1e
LCCardenas <44859902+LCCardenas@users.noreply.github.com>
2020-04-09 11:51:27 +0200

    components created. Refactoring

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../public/styles.css";
+import Heading from "./Heading.jsx";
+import Listing from "./Listing";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Listing />
+    </div>
+  );
+}
+
+export default App;
